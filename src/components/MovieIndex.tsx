1f11a6660fccae9441ce9e72dbfbc67b5a283c14
Jeffery Vincent <jevinc@ZADG4CJHD3.cbn.local>
2019-05-12 21:10:29 -0400

    switched to hash router

--- src/components/MovieIndex.tsx
+++ src/components/MovieIndex.tsx
@@ -65,7 +65,7 @@ const MovieIndex: React.FC<MovieIndexProps> = ({match, history}) => {
       }
     }, [state.filter, pager]);
 
-    console.log(state);
+    //console.log(state);
 
     return (
       <Context.Provider value={dispatch}>
